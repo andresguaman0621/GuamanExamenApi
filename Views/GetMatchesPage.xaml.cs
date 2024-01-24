9c3299a45f207e2ead470cdef4e219e3c8c7553a
Andres Alejandro Guaman Taco <andres.guaman.taco@udla.edu.ec>
2024-01-24 08:23:13 -0500

    Obtencion de datos API correcta

--- Views/GetMatchesPage.xaml.cs
+++ Views/GetMatchesPage.xaml.cs
@@ -1,54 +1,54 @@
 using GuamanExamenApi.Models;
-using System.Net.Sockets;
+using System.Net.Http;
 using System.Text.Json;
 using Newtonsoft.Json;
 
-namespace GuamanExamenApi.Views;
-
-public partial class GetMatchesPage : ContentPage
+namespace GuamanExamenApi.Views
 {
-    public GetMatchesPage()
+    public partial class GetMatchesPage : ContentPage
     {
-        InitializeComponent();
-    }
-
-   
-
-    private async void Button_Clicked(object sender, EventArgs e)
-    {
-        try
+        public GetMatchesPage()
         {
-            string apiKey = "a43d9381662e4720a89ec322e5e5ab15";
+            InitializeComponent();
+        }
 
-            // Configura las fechas en el formato correcto (YYYY-MM-DD)
-            DateTime startDate = new DateTime(2024, 1, 24);
-            DateTime endDate = new DateTime(2024, 1, 25);
-
-            string apiUrl = $"https://api.football-data.org/v4/matches/?apikey={apiKey}&dateFrom={startDate:yyyy-MM-dd}&dateTo={endDate:yyyy-MM-dd}";
-
-            using (HttpClient client = new HttpClient())
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
             {
-                // Agrega la clave de API como encabezado
-                client.DefaultRequestHeaders.Add("X-Auth-Token", apiKey);
+                string apiKey = "a43d9381662e4720a89ec322e5e5ab15";
 
-                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                //Fechas
+                DateTime startDate = new DateTime(2024, 1, 23);
+                DateTime endDate = new DateTime(2024, 1, 25);
 
-                if (response.IsSuccessStatusCode)
+                string apiUrl = $"https://api.football-data.org/v4/matches/?apikey={apiKey}&dateFrom={startDate:yyyy-MM-dd}&dateTo={endDate:yyyy-MM-dd}";
+
+                using (HttpClient client = new HttpClient())
                 {
-                    string jsonResponse = await response.Content.ReadAsStringAsync();
-                    // Deserializa la respuesta en tu modelo de datos
-                    var matchesData = JsonConvert.DeserializeObject<Soccer.Rootobject>(jsonResponse);
+                    
+                    client.DefaultRequestHeaders.Add("X-Auth-Token", apiKey);
 
-                    // Accede a la lista de partidos bajo la propiedad matches en el objeto resultSet
-                    MatchesListView.ItemsSource = matchesData.matches;
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        
+                        var matchesData = JsonConvert.DeserializeObject<Soccer.Rootobject>(jsonResponse);
+
+                        
+                        MatchesListView.ItemsSource = matchesData.matches;
+                    }
+
                 }
-
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
+
 
     }
 
